cb9130598411be6e3ba76bb28da3c41ad5445934
swastiktiwari1 <36890412+swastiktiwari1@users.noreply.github.com>
2020-02-22 09:41:53 +0530

    Create assignment_problem_dp_bitwise.cpp

---
+++ assignment_problem_dp_bitwise.cpp
@@ -0,0 +1,80 @@
+#include<bits/stdc++.h>
+#include<string>
+using namespace std;
+#define fastIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define int long long int
+#define fi first
+#define se second
+#define pub push_back
+#define mkp make_pair
+#define pi pair<int,int>0
+#define push push
+#define all(v) (v).begin(), (v).end()
+#define rep(i, l, r) for(int i=(int)(l);i<(int)(r);i++)
+#define repd(i, l, r) for (int i=(int)(l);i>=(int)(r);i--)
+// int power(int x, unsigned int y){int res = 1;while (y > 0){ if (y & 1){res = res*x;} y = y>>1;x = x*x;}return res;}
+// int powermod(int x, unsigned int y, int p){int res = 1;x = x % p;while (y > 0){if (y & 1){res = (res*x) % p;}y = y>>1; x = (x*x) % p;}return res;}
+#define print2d(mat,n,m){for(int i=0;i<(int)(n);i++){for(int j=0;j<(m);j++){cout<<mat[i][j]<<" ";}cout<< endl;}}
+#define print1d(mat,n){for(int i=0;i<(int)(n);i++)cout<<mat[i]<<" ";cout<<endl;}
+#define clr(a,x) memset(a,x,sizeof(a))
+#define rr(v) for(auto &val:v)
+#define print(v) for (const auto itr : v){cout<<itr<<' ';}cout<<endl;
+#define init(v,x) for (auto &itr:v){itr=x;}
+#define ln length()
+#define sz size()
+#define inf 1e18;
+#define elif else if
+int countsb(int x)
+{
+    int c=0;
+    while(x)
+    {
+        x=x&(x-1);
+        c+=1;
+    }
+    return c;
+}
+
+int32_t main()
+{
+    // fastIO
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+    #endif
+    int cost[10][10];
+    int n,nosb,mask,i;
+    cin>>n;
+    rep(i,0,n)
+    {
+        rep(j,0,n)
+        {
+            cin>>cost[i][j];
+        }
+    }
+    int dp[10000];
+    clr(dp,0);
+    rep(i,1,pow(2,n))
+    {
+        dp[i]=inf;
+    }
+    // print1d(dp,pow(2,n))
+
+    rep(mask,0,pow(2,n))
+    {
+        // cout<<mask<<endl;
+        nosb=countsb(mask);
+        rep(j,0,n)
+        {
+            
+            if(!(mask&(1<<j)))
+            {
+                i=mask|(1<<j);
+                // cout<<"idhar? "<<mask<<" "<<nosb<<endl;
+                dp[i]=min(dp[i],dp[mask]+cost[nosb][j]);
+                // print1d(dp,pow(2,n))
+            }
+        }
+    }
+    print1d(dp,pow(2,n))
+
+}
